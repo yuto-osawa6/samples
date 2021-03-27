469464f77dd22e2fa340b42e6f8a526ebf0c90ef
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-03-28 02:22:17 +0900

    homes(toppage,css)

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_27_055208) do
+
+  create_table "homes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
