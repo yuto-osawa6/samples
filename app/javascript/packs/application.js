d899ca1ba5a1f0c3229ff3374240203be5f724ff
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-04-04 06:52:10 +0900

    end uiux

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,4 +19,6 @@ require("channels")
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
+import '@fortawesome/fontawesome-free/js/all';
+
 import '../stylesheets/application.scss'
---
+++ app/javascript/packs/swiper/swiper.js
@@ -0,0 +1,90 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1.5,
+  centeredSlides : true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+		renderBullet: function (index, className) {
+			//中に数字を表示
+			return '<span class="' + className + '">' + (index + 1) + '</span>';
+		}
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+const swiper2 = new Swiper('.swiper-container2', {
+  // Optional parameters
+
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 4,
+  centeredSlides : true,
+  allowTouchMove: false, // スワイプ操作をできないようにする
+  // effect: 'slide',    //切り替わるときのアニメーション
+      speed: 3000,        //画像の切替スピード
+      autoplay: {         //自動で動かす
+        delay: 0,
+        disableOnInteraction: false
+      },
+
+
+  // direction: 'horizontal',
+  // loop: true,
+  // slidesPerView: 1.5,
+  // centeredSlides : true,
+  // // speed: 3000,        //画像の切替スピード
+  // //     autoplay: {         //自動で動かす
+  // //       delay: 0,
+  // //       disableOnInteraction: false
+  // //     },
+
+  // // speed: 8000,
+  // // slidesPerView: '1',
+  // // loop: true,
+  // // allowTouchMove: false, // スワイプ操作をできないようにする
+  // // autoplay: {
+  // //   delay: 0,　// 0にすることで流れ続けるようになる
+  // //   disableOnInteraction: false,
+  // // },
+
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+	// 	type: 'bullets',
+	// 	clickable: true,
+	// 	renderBullet: function (index, className) {
+	// 		//中に数字を表示
+	// 		return '<span class="' + className + '">' + (index + 1) + '</span>';
+	// 	}
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
