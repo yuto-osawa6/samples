dc3558153a521a53c4aeec88effb220d90f12e82
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-04-10 06:32:48 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,136 +7,136 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# User.create!(
-#              email: "atgmw628@gmail.com",
-#              password:  "6708atgmw",
-#              password_confirmation: "6708atgmw",
-#              admin: true)
+User.create!(
+             email: "atgmw628@gmail.com",
+             password:  "6708atgmw",
+             password_confirmation: "6708atgmw",
+             admin: true)
 
-            #  User.create!(
-            #   username: "atgmw78",
-            #   email: "atgmw628@gmail.com",
-            #   password:  "6708atgmw",
-            #   password_confirmation: "6708atgmw",
-            #   admin: true)
+             User.create!(
+              username: "atgmw78",
+              email: "atgmw628@gmail.com",
+              password:  "6708atgmw",
+              password_confirmation: "6708atgmw",
+              admin: true)
 
 
-              # Ahome.create!(
-              #   title:  "あなたのチームに",
-              #   title2: "クリエイターを最短で",
-              #   discribe: "全く新しい 月額制 の受託スタイル",
-              #   button_right: "お問い合わせ",
-              #   button_left: "月額プランをみる",
+              Ahome.create!(
+                title:  "あなたのチームに",
+                title2: "クリエイターを最短で",
+                discribe: "全く新しい 月額制 の受託スタイル",
+                button_right: "お問い合わせ",
+                button_left: "月額プランをみる",
 
-              #   url_right: "",
-              #   url_left: ""
-              # )
+                url_right: "",
+                url_left: ""
+              )
 
-              # Bhome.create!(
-              #   title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
-              #   title2:"動画マーケティング総合支援サービス。",
-              #   discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
+              Bhome.create!(
+                title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
+                title2:"動画マーケティング総合支援サービス。",
+                discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
 
-              # )
+              )
 
-              # Bbhome.create([{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"} ])
+              Bbhome.create([{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",bhome_id:"1"} ])
 
-              # Chome.create!(
-              #   title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
-              #   title2:"動画マーケティング総合支援サービス。",
-              #   discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
+              Chome.create!(
+                title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
+                title2:"動画マーケティング総合支援サービス。",
+                discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
 
-              # )
+              )
 
-              # Dhome.create!(
-              #   title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
-              #   title2:"動画マーケティング総合支援サービス。",
-              #   discribe2:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
-              #   discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
+              Dhome.create!(
+                title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
+                title2:"動画マーケティング総合支援サービス。",
+                discribe2:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
+                discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
 
-              # )
+              )
 
-              # Ehome.create!(
+              Ehome.create!(
 
-              #   title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
-              #   discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
+                title:"CINEMATO（シネマト）は、動画で企業の経営課題を解決する",
+                discribe:"動画制作・映像制作、広告運用支援、広告パフォーマンス改善、コンサルティングをワンストップで対応できます。",
 
-              # )
+              )
 
-              # Fhome.create!(
-              #   title:"メリット",
-              #   title2:"価値を映す",
-              #   discribe:"クレーンやレール、サーモグラフィや高速度撮影用カメラなどを使用し、通常の人間の視点では、見ることのできない映像を表現します。"
+              Fhome.create!(
+                title:"メリット",
+                title2:"価値を映す",
+                discribe:"クレーンやレール、サーモグラフィや高速度撮影用カメラなどを使用し、通常の人間の視点では、見ることのできない映像を表現します。"
 
-              # )
+              )
 
-              # Ffhome.create!([{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"} ])
+              Ffhome.create!([{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"},{title: "動画広告配信",discribe:"Youtube、GSN、YDN、SNS、TikTok、タクシー広告など、目的に合わせて適切な媒体の選定・配信を行います。",fhome_id:"1"} ])
 
-              # Ihome.create!(
-              #   title:"豊富なプランをご用意。",
-              #   title2:"特定領域だけの対応から全領域の一貫対応まで、",
-              #   title3:"あなたのチームに最適なプランをお選びください。"
-              # )
+              Ihome.create!(
+                title:"豊富なプランをご用意。",
+                title2:"特定領域だけの対応から全領域の一貫対応まで、",
+                title3:"あなたのチームに最適なプランをお選びください。"
+              )
 
-            #   Iihome.create!([{
-            #     title:"Plan.A",
-            #     discribe:"デザイナーのみ、エンジニアのみなど チーム体制に合わせた必要領域の 補強に特化したプラン",
-            #     colum1:"チーム状況に応じたスポット対応",
-            #     colum2:"お好きな領域の選択が可能",
-            #     colum3:"TAGの試験的な導入として人気",
-            #     price:"月額 ¥300,000 (3ヶ月更新)",
-            #     ihome_id:1
-            #   },
-            #   {
-            #     title:"Plan.A",
-            #     discribe:"デザイナーのみ、エンジニアのみなど チーム体制に合わせた必要領域の 補強に特化したプラン",
-            #     colum1:"チーム状況に応じたスポット対応",
-            #     colum2:"お好きな領域の選択が可能",
-            #     colum3:"TAGの試験的な導入として人気",
-            #     price:"月額 ¥300,000 (3ヶ月更新)",
-            #     ihome_id:1
+              Iihome.create!([{
+                title:"Plan.A",
+                discribe:"デザイナーのみ、エンジニアのみなど チーム体制に合わせた必要領域の 補強に特化したプラン",
+                colum1:"チーム状況に応じたスポット対応",
+                colum2:"お好きな領域の選択が可能",
+                colum3:"TAGの試験的な導入として人気",
+                price:"月額 ¥300,000 (3ヶ月更新)",
+                ihome_id:1
+              },
+              {
+                title:"Plan.A",
+                discribe:"デザイナーのみ、エンジニアのみなど チーム体制に合わせた必要領域の 補強に特化したプラン",
+                colum1:"チーム状況に応じたスポット対応",
+                colum2:"お好きな領域の選択が可能",
+                colum3:"TAGの試験的な導入として人気",
+                price:"月額 ¥300,000 (3ヶ月更新)",
+                ihome_id:1
 
-            #   },
-            #   {
-            #     title:"Plan.A",
-            #     discribe:"デザイナーのみ、エンジニアのみなど チーム体制に合わせた必要領域の 補強に特化したプラン",
-            #     colum1:"チーム状況に応じたスポット対応",
-            #     colum2:"お好きな領域の選択が可能",
-            #     colum3:"TAGの試験的な導入として人気",
-            #     price:"月額 ¥300,000 (3ヶ月更新)",
-            #     ihome_id:1
-            #   }
-            # ])
+              },
+              {
+                title:"Plan.A",
+                discribe:"デザイナーのみ、エンジニアのみなど チーム体制に合わせた必要領域の 補強に特化したプラン",
+                colum1:"チーム状況に応じたスポット対応",
+                colum2:"お好きな領域の選択が可能",
+                colum3:"TAGの試験的な導入として人気",
+                price:"月額 ¥300,000 (3ヶ月更新)",
+                ihome_id:1
+              }
+            ])
 
-          #   Aahome.create!(
-          #   [{
-          #     menu:"SERVICE",
-          #     ahome_id:1
-          #   },{
-          #     menu:"ABOUT",
-          #     ahome_id:1
-          #   },{
-          #     menu:"PERFORMANCE",
-          #     ahome_id:1
-          #   },{
-          #     menu:"MERIT",
-          #     ahome_id:1
+            Aahome.create!(
+            [{
+              menu:"SERVICE",
+              ahome_id:1
+            },{
+              menu:"ABOUT",
+              ahome_id:1
+            },{
+              menu:"PERFORMANCE",
+              ahome_id:1
+            },{
+              menu:"MERIT",
+              ahome_id:1
 
-          #   },{
-          #     menu:"PLAN",
-          #     ahome_id:1
-          #   },
-          #   {
-          #     menu:"BLOG",
-          #     ahome_id:1
-          #   },
-          #   {
-          #     menu:"お問い合わせ",
-          #     ahome_id:1
-          #   }   
+            },{
+              menu:"PLAN",
+              ahome_id:1
+            },
+            {
+              menu:"BLOG",
+              ahome_id:1
+            },
+            {
+              menu:"お問い合わせ",
+              ahome_id:1
+            }   
           
-          # ]
-          #   )
+          ]
+            )
 
               
 
