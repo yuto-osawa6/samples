139e7b4dd29152fefcb6daccffd95154cf43bfe4
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-04-12 16:23:45 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :ahome
 
   def after_sign_in_path_for(resource)
-    user_path(resource.id)
+    admin_homes_path
   end
 
   protected
