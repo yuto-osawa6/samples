0d93e39303c4a3cf3ec0c448cfd8a560978616d9
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-04-13 21:28:57 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -77,6 +78,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
+  # config.active_storage.service = :amazon
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
