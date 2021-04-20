b97e2331c0d671568f2f8f4c9505b9f220646acb
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-04-21 07:30:57 +0900

    Update hover.js

--- app/javascript/javascripts/hover.js
+++ app/javascript/javascripts/hover.js
@@ -18,29 +18,46 @@ $(document).on('turbolinks:load', function(){
 //       $(this).removeClass('News__contents__lists__cover__second')
 //   }
 // );
-$('.News__contents__lists__cover').hover(
-  function() {
-      
-     const blogsId = $(this).data('b2blogs__id');
-     $('.blogslie'+blogsId).css('opacity', '0.6');
+const mql = window.matchMedia('(min-width: 769px)');
+const handleMediaQuery = function(mql) {
+  if (mql.matches) {
+    // 769px以上の場合の処理
+    console.log('769px以上');
+    $('.News__contents__lists__cover').hover(
+      function() {
+          
+         const blogsId = $(this).data('b2blogs__id');
+         $('.blogslie'+blogsId).css('opacity', '0.6');
+    
+          //マウスカーソルが重なった時の処理
+          // $('.blogslie'+blogsId).css('transform', 'translateY(-10px)');
+          // $('.blogslie'+blogsId).addClass('News__contents__lists__cover__second')
+          // $(this).css('transform', 'translateY(-10px)');
+          // $(this).addClass('News__contents__lists__cover__second')
+         
+          
+          
+      },
+      function() {
+          const blogsId = $(this).data('b2blogs__id');
+          $('.blogslie'+blogsId).css('opacity', '1');
+    
+          // $('.blogslie'+blogsId).css('transform', 'translateY(10px)');
+          // $('.blogslie'+blogsId).removeClass('News__contents__lists__cover__second')
+          //マウスカーソルが離れた時の処理
+          // $(this).css('transform', 'translateY(10px)');
+          // $(this).removeClass('News__contents__lists__cover__second')
+      }
+    );
 
-      //マウスカーソルが重なった時の処理
-      // $('.blogslie'+blogsId).css('transform', 'translateY(-10px)');
-      // $('.blogslie'+blogsId).addClass('News__contents__lists__cover__second')
-      // $(this).css('transform', 'translateY(-10px)');
-      // $(this).addClass('News__contents__lists__cover__second')
-      
-  },
-  function() {
-      const blogsId = $(this).data('b2blogs__id');
-      $('.blogslie'+blogsId).css('opacity', '1');
 
-      // $('.blogslie'+blogsId).css('transform', 'translateY(10px)');
-      // $('.blogslie'+blogsId).removeClass('News__contents__lists__cover__second')
-      //マウスカーソルが離れた時の処理
-      // $(this).css('transform', 'translateY(10px)');
-      // $(this).removeClass('News__contents__lists__cover__second')
+  } else {
+    // 769px未満の場合の処理
+    console.log('769px未満');
   }
-);
+};
+
+mql.addListener(handleMediaQuery);
+handleMediaQuery(mql);
 
 });
