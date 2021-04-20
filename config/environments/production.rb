d4cba8ea3da80f3b8a11e053bb59cd1b401eed82
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-04-21 08:04:57 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:            Rails.application.credentials[:gmail],
+  password:             Rails.application.credentials[:gpassword],
+ 
+  authentication:       'plain',
+  enable_starttls_auto:  true
+}
 end
