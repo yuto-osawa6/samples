4b88a892c8d55faf5a6c3bf7236798e684774b12
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-05-07 18:43:40 +0900

    Update video_uploader.rb

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,14 +4,14 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
-  # if Rails.env.production?
-  #   storage :fog
-  # else
-  #   storage :file
-  # end
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
