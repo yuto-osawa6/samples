9b172fb02b71fc5f2c59f8a673bba83103e9435d
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-07-01 08:47:15 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -102,28 +102,28 @@
               menu:"SERVICE",
               mainmenu:"サービス紹介",
               ahome_id:1,
-              link:"#aa"
+              link:"#aa",
             },{
               menu:"WORKS",
               mainmenu:"映像制作実績",
               ahome_id:1,
-              link:"#ac"
+              link:"#ac",
             },{
               menu:"INTERVIEW",
               mainmenu:"お客様の声",
               ahome_id:1,
-              link:"#ad"
+              link:"#ad",
             },{
               menu:"PRICE",
               mainmenu:"料金",
               ahome_id:1,
-              link:"#ae"
+              link:"#ae",
 
             },{
               menu:"FILM MONKEY",
               mainmenu:"フィルムモンキーとは",
               ahome_id:1,
-              link:"#ab"
+              link:"#ab",
             },
             # {
             #   menu:"BLOG",
@@ -136,7 +136,7 @@
               menu:"CONTACT",
               mainmenu:"お問い合わせ",
               ahome_id:1,
-              link:"#ag"
+              link:"#ag",
             }   
           
           ]
