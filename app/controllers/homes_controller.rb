5bb19acb47005e28236b3980391b51ad1c55bdf2
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-07-01 22:35:01 +0900

    n

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,12 @@
 class HomesController < ApplicationController
   def index
-    @ahomes = Ahome.find(1)
-    @bhomes = Bhome.find(1)
+    @ahomes = Ahome.includes(:aahomes).find(1)
+    @bhomes = Bhome.includes(:bbhomes).find(1)
     @chomes = Chome.find(1)
     @dhomes = Dhome.find(1)
-    @ehomes = Ehome.find(1)
-    @fhomes = Fhome.find(1)
-    @ihomes = Ihome.find(1)
+    @ehomes = Ehome.includes(:eehomes).find(1)
+    @fhomes = Fhome.includes(:ffhomes).find(1)
+    @ihomes = Ihome.includes(:iihomes).find(1)
     # @blogs = Blog.new
     @blogsfs = Blog.all
 
