c9735183d5e5579f0b7536c7ef070722e3e08b31
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-07-01 21:10:53 +0900

    s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,11 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      # aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      # aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+
+      aws_access_key_id: Rails.application.credentials.dig(:aws2, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws2, :secret_access_key),
       region: "ap-northeast-1"
     }
     config.fog_directory = "backetsamples"
